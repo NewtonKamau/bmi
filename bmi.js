008e880f1d2a14fd18d24381817a25d59ba3cf33
Newton <kamaunewton78@gmail.com>
2020-07-01 15:51:31 +0300

    used body parser to read data from the user input, that is weight and height, and calculate the bmi

--- bmi.js
+++ bmi.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/bmiCalculator.html"));
+app.post("/", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = Math.floor(weight / (height * height));
+  res.send("Your bmi is  " + bmi);
+});
 
 app.listen(port, () => console.log("server running on port 3000"));
